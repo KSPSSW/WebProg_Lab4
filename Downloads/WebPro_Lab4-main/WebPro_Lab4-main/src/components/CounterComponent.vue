f3720cf231e07b74896c6e6a618f050a8dca8e2e
Kritsaphong Saengsawang <66160290@go.buu.ac.th>
2025-01-14 16:02:12 +0700

    fix POS page

---
+++ Downloads/WebPro_Lab4-main/WebPro_Lab4-main/src/components/CounterComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>{{ num }}</div>
+  <div>{{ data }}</div>
+  <div>
+    <button @click="dec">-</button>
+    <button @click="inc">+</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted /*onBeforeUpdate, onUpdated*/ } from 'vue';
+
+const props = defineProps<{
+  num: number
+}>()
+
+const emit = defineEmits<{
+  update: [num: number]
+}>()
+
+onMounted(() => {
+  data.value = props.num
+})
+
+/*
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+
+onUpdated(() => {
+  console.log('onUpdated')
+}) */
+
+const data = ref(2)
+
+const inc = () => {
+  data.value++
+  emit('update', data.value)
+}
+
+const dec = () => {
+  data.value--
+  emit('update', data.value)
+}
+</script>
